927ba5a76cd51f4f4c9977efbdff6b2bbb4d7384
Emanuel Colombo <colomboemanuel@gmail.com>
2015-06-25 19:38:26 +0100

    php form and some issues solved

--- form.php
+++ form.php
@@ -1,29 +1,21 @@
-	[php]
 	<?php
+	‘vendor/autoload.php’;
 	$nombre = $_POST[‘nombre’];
 	$email = $_POST[‘email’];
 	$mensaje = $_POST[‘mensaje’];
 	$telefono = $_POST[‘inputPhone’];
-	$para = ‘colomboemanue@gmail.com';
-	$titulo = ‘Nueva consulta de la web';
+	$para = ‘colombopisos@gmail.com’;
+	$titulo = ‘Nueva consulta de la web’;
 	$header = ‘From: ‘ . $email;
 	$msjCorreo = "Nombre: $nombre\n E-Mail: $email\n  Telefóno: $telefono\n Mensaje:\n $mensaje";
 
 
 
-	if ($_POST[‘submit’]) {
-	if (mail($para, $titulo, $msjCorreo, $header)) {
-	echo "<script language=’javascript’>
-	alert(‘Mensaje enviado, muchas gracias.’);
-	window.location.href = ‘http://colombopisos.com.ar';
-	</script>";
-	} else {
-	echo ‘Falló el envio';
-	}
-	}
+	$transport = Swift_SmtpTransport::newInstance(‘smtp.gmail.com’, 587,‘tls’);
+	$transport->setUsername(‘colombopisos@gmail.com’);
+	$transport->setPassword(‘’);
 
 	?>
-	[/php]
 
 
 
